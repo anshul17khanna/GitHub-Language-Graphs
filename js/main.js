58cd9f65002a811a4b0b7727e285d18870171c46
Anshul Khanna <anshul17khanna@gmail.com>
2016-06-23 16:36:57 +0530

    static example

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
-(function init(){
+function init(){
+    var repos = [];
+    $.getJSON("https://api.github.com/users/anshul17khanna/repos?callback=?", function(data){
+        $(data.data).each(function(i,d){
+            if(d.language===null)
+                repos.push("null");
+            else
+                repos.push(d.language);
+        });
+        alert(repos);
+    });
 
-  var Chart = require('Chart.js');
+    requirejs(["js/Chart.js"], function(Chart) {
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [200,145,53,9,164],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+            }
+        });
+    });
+}
 
-});
+window.onload = init;
