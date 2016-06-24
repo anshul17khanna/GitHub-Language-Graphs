3a4bf90cdcbf2b828daf76a432ac7d0fe64adae1
Anshul Khanna <anshul17khanna@gmail.com>
2016-06-25 01:57:30 +0530

    + line chart

--- js/main.js
+++ js/main.js
@@ -5,20 +5,37 @@ var language_keys = [];
 var language_values = [];
 
 function init(){
-  var barData = {
-    labels: language_keys,
-    datasets: [{
-        label: "My First dataset",
-        fillColor: "rgba(247, 70, 74,0.5)",
-        strokeColor: "rgba(247, 70, 74,0.8)",
-        highlightFill: "rgba(247, 70, 74,0.75)",
-        highlightStroke: "rgba(247, 70, 74,1)",
-        data: language_values
-    }]
-  };
+    var barData = {
+        labels: language_keys,
+        datasets: [{
+            label: "My First dataset",
+            fillColor: "rgba(247, 70, 74,0.5)",
+            strokeColor: "rgba(247, 70, 74,0.8)",
+            highlightFill: "rgba(247, 70, 74,0.75)",
+            highlightStroke: "rgba(247, 70, 74,1)",
+            data: language_values
+        }]
+    };
 
-  var barC = $("#barChart").get(0).getContext("2d");
-  var barChart = new Chart(barC).Bar(barData, { responsive: true, maintainAspectRatio: false });
+    var barC = $("#barChart").get(0).getContext("2d");
+    var barChart = new Chart(barC).Bar(barData, { responsive: true, maintainAspectRatio: false });
+
+    var lineData = {
+			  labels: language_keys,
+			  datasets: [{
+				label: "My First dataset",
+				fillColor: "rgba(0, 204, 102,0.2)",
+				strokeColor: "rgba(0, 204, 102,1)",
+				pointColor: "rgba(0, 204, 102,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(0, 204, 102,1)",
+				data: language_values
+		}]
+};
+
+    var lineC = $("#lineChart").get(0).getContext("2d");
+		var lineChart = new Chart(lineC).Line(lineData, { responsive: true, maintainAspectRatio: false });
 }
 
 window.onload = init;
