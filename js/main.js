e43aaf8f6c7d4da0014f3afce3883b8a84fb80f4
Anshul Khanna <anshul17khanna@gmail.com>
2016-06-25 00:46:15 +0530

    + bar chart

--- js/main.js
+++ js/main.js
@@ -1,54 +1,42 @@
-function init(){
-    var repos = [];
-    $.getJSON("https://api.github.com/users/anshul17khanna/repos?callback=?", function(data){
-        $(data.data).each(function(i,d){
-            if(d.language===null)
-                repos.push("null");
-            else
-                repos.push(d.language);
-        });
-        alert(repos);
-    });
+var arr = [];
+var repos = [];
+var languages_data = {};
+var language_keys = [];
+var language_values = [];
 
-    requirejs(["js/Chart.js"], function(Chart) {
-        var ctx = document.getElementById("myChart");
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [200,145,53,9,164],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-            }
-        });
-    });
+function init(){
+  var barData = {
+    labels: language_keys,
+    datasets: [{
+        label: "My First dataset",
+        fillColor: "rgba(247, 70, 74,0.5)",
+        strokeColor: "rgba(247, 70, 74,0.8)",
+        highlightFill: "rgba(247, 70, 74,0.75)",
+        highlightStroke: "rgba(247, 70, 74,1)",
+        data: language_values
+    }]
+  };
+
+  var barC = $("#barChart").get(0).getContext("2d");
+  var barChart = new Chart(barC).Bar(barData, { responsive: true, maintainAspectRatio: false });
 }
 
 window.onload = init;
+
+$.getJSON("https://api.github.com/users/anshul17khanna/repos?callback=?", function(data){
+
+    $(data.data).each(function(i,d){
+        repos.push(d.name);
+        if(!languages_data[d.language])
+            languages_data[d.language] = parseInt('1');
+        else
+            languages_data[d.language] += 1;
+    });
+
+    for (var k in languages_data){
+        if (languages_data.hasOwnProperty(k)){
+            language_keys.push(k);
+            language_values.push(languages_data[k]);
+        }
+    }
+});
